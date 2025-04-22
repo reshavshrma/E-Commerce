0fdf2a6e67d33cf3361a4b9d6b7a62feb6266d75
Reshav Sharma <2022bit061@sggs.ac.in>
2025-04-23 04:28:01 +0530

    final update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,13 @@
-export default defineConfig({
+// vite.config.js
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
+const tailwindcss = require('@tailwindcss/vite');
+
+module.exports = defineConfig({
   base: '/',
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'dist', // Change to 'dist' so the build output is placed here
-    emptyOutDir: true, // Clears old build files before a new build
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 });
