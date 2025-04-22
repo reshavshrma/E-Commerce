6ee46909c866ccb9a1fddebe30522c87bfae967a
Reshav Sharma <2022bit061@sggs.ac.in>
2025-04-23 04:25:45 +0530

    final update 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
   base: '/',
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: '../server/public', // This builds React into backend/public folder
-    emptyOutDir: true,          // Clears old build files before new build
+    outDir: 'dist', // Change to 'dist' so the build output is placed here
+    emptyOutDir: true, // Clears old build files before a new build
   },
 });
